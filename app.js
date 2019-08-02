69077c53ed0dfec7b39070af978467215bc4d340
picpoint <picpoint@yandex.ru>
2019-08-02 16:55:13 +0300

    make many pages

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
 
 app.get('/', (req, res) => {
-	res.type('text/html');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
 
 
 app.get('/about', (req, res) => {
-	res.type('text/html');
-	res.send('About ...');
+	res.render('about');
 });
 
 
-app.use((req, res) => {
-	res.type('text/plain');
+app.use((req, res, next) => {
 	res.status(404);
-	res.send('404 - page not found ...');
+	res.render('404');
 });
 
 
 app.use((err, req, res, next) => {
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Internal Server Error');
+	res.send('500');
 });
 
 
