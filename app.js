16d8cbfe3d2033b96df844fcb7bf3e2610ae48a1
rmtar <picpoint@yandex.ru>
2019-08-07 21:07:28 +0300

    work with page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const skils = require('./public/lib/skils.js');
 const skilsRandom = require('./public/lib/skilsRandom.js');
 const mocha = require('mocha');
 const chai = require('chai');
+const bodyParser = require('body-parser');
 
 
 
@@ -13,6 +14,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + 'public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use(function(req, res, next){
@@ -34,6 +36,7 @@ app.get('/headers', (req, res) => {
 
 app.get('/', (req, res) => {
 	res.render('home');
+	console.log(req.session);
 });
 
 
@@ -69,6 +72,44 @@ app.use((err, req, res, next) => {
 });
 
 
+app.use((req, res, next) => {
+	if (!res.locals.partials) {
+		res.locals.partials = {};
+	}
+	res.locals.partials.weatherContext = getWeatherData();
+	next();
+});
+
+
 app.listen(app.get('port'), () => {
 	console.log('server has been starting to port: ' + app.get('port') );
 });
+
+
+function getWeatherData() {
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Clouds',
+				temp: '54.1 F (12.3 C)'
+			},
+			{
+				name: 'Бенд',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Малооблачно',
+				temp: '55.0 F (12.8 C)'
+			},
+			{
+				name: 'Манзанита',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Небольшой дождь',
+				temp: '55.0 F (12.8 C)'
+			}
+		]
+	};
+}
